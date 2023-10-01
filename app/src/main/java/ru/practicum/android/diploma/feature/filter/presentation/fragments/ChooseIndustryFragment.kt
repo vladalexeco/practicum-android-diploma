13fb3eef4f3e68932943c4fc88f14008420bfc8e
vladalexeco <vladalexeco86@gmail.com>
2023-10-01 23:38:45 +0300

    task-branch-9: Добавлен экран "Выбор региона".

---
+++ app/src/main/java/ru/practicum/android/diploma/feature/filter/presentation/fragments/ChooseIndustryFragment.kt
@@ -0,0 +1,35 @@
+package ru.practicum.android.diploma.feature.filter.presentation.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.databinding.FragmentChooseIndustryBinding
+import ru.practicum.android.diploma.databinding.FragmentSearchBinding
+
+class ChooseIndustryFragment : Fragment() {
+
+    private var _binding: FragmentChooseIndustryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseIndustryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
