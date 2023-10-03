d11d4494a7259178b797277a9be095cce1d393d8
Maxim <maximus616161@gmail.com>
2023-10-03 21:45:57 +0700

    Добавил network-клиент и некоторые сопутствующие классы

---
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/data/network/RetrofitNetworkClient.kt
@@ -0,0 +1,78 @@
+package ru.practicum.android.diploma.feature.search.data.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import ru.practicum.android.diploma.feature.search.data.NetworkClient
+import ru.practicum.android.diploma.feature.search.data.Response
+
+class RetrofitNetworkClient (private val context: Context,
+                             private val hhApi: HeadHunterApi) : NetworkClient {
+
+    override suspend fun getVacancies(dto: Any): Response {
+        if (!isConnected()) {
+            return Response().apply { resultCode = -1 }
+        }
+        if (dto !is SearchRequest) {
+            return Response().apply { resultCode = 400 }
+        }
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = hhApi.getVacancies(dto.data)
+                response.apply { resultCode = 200 }
+            } catch (e:Throwable) {
+                Response().apply { resultCode = 500}
+            }
+        }
+    }
+
+    override suspend fun getVacancy(dto: Any): Response {
+        if (!isConnected()) {
+            return Response().apply { resultCode = -1 }
+        }
+        if (dto !is SearchRequest) {
+            return Response().apply { resultCode = 400 }
+        }
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = hhApi.getVacancy(dto.data)
+                response.apply { resultCode = 200 }
+            } catch (e:Throwable) {
+                Response().apply { resultCode = 500}
+            }
+        }
+    }
+
+    override suspend fun getSimilarVacancies(dto: Any): Response {
+        if (!isConnected()) {
+            return Response().apply { resultCode = -1 }
+        }
+        if (dto !is SearchRequest) {
+            return Response().apply { resultCode = 400 }
+        }
+        return withContext(Dispatchers.IO) {
+            try {
+                val response = hhApi.getSimilarVacancies(dto.data)
+                response.apply { resultCode = 200 }
+            } catch (e:Throwable) {
+                Response().apply { resultCode = 500}
+            }
+        }
+    }
+
+    private fun isConnected(): Boolean {
+        val connectivityManager = context.getSystemService(
+            Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            }
+        }
+        return false
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/domain/VacancyRepository.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.feature.search.domain
+
+import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.core.util.Resource
+
+interface VacancyRepository {
+    //fun getVacancies(expression: String): Flow<Resource<List<Vacancy>>>
+    //fun getVacancy(expression: String): Flow<Resource<Vacancy>>
+    //fun getSimilarVacancies(expression: String): Flow<Resource<List<Vacancy>>>
+}
