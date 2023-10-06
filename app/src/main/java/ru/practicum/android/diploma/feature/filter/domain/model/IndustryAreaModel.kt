2bdd8fcefcf96c1b240381118e8ac95cbf36497a
Chernou <44603127+Chernou@users.noreply.github.com>
2023-10-06 18:13:33 +0300

    Abstract вместо open IndustryAreaModel

--- app/src/main/java/ru/practicum/android/diploma/feature/filter/domain/model/IndustryAreaModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filter/domain/model/IndustryAreaModel.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.feature.filter.domain.model
 
-open class IndustryAreaModel(
+abstract class IndustryAreaModel(
     open val id: String,
     open val name: String
 ) {
