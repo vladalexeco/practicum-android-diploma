a62a52645d2450b85d809f806ee6a429d7921fb2
Maxim <maximus616161@gmail.com>
2023-10-07 19:52:40 +0700

    Отредачил метод получения вакансии по айди.

--- app/src/main/java/ru/practicum/android/diploma/feature/search/data/network/HeadHunterApi.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/data/network/HeadHunterApi.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.feature.search.data.network
 
+import com.usunin1994.headhunterapi.data.dtomodels.VacancyDtoFull
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Path
@@ -16,7 +17,7 @@ interface HeadHunterApi {
     suspend fun getVacancies(@Query("text") text: String): VacanciesDtoResponse
 
     @GET("/vacancies/{vacancy_id}")
-    suspend fun getVacancy(@Path("vacancy_id") id: String): VacancyDtoResponse
+    suspend fun getVacancy(@Path("vacancy_id") id: String): VacancyDtoFull
 
     @GET("/vacancies/{vacancy_id}/similar_vacancies")
     suspend fun getSimilarVacancies(@Path("vacancy_id") id: String): VacanciesDtoResponse
