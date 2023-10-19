51f1644219d691f1547d85b822308fc24d889302
Vlad <tchervladislav@yandex.ru>
2023-10-19 22:51:12 +0700

    fix

--- app/src/main/java/ru/practicum/android/diploma/feature/search/presentation/fragments/SearchFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/presentation/fragments/SearchFragment.kt
@@ -1,8 +1,6 @@
 package ru.practicum.android.diploma.feature.search.presentation.fragments
 
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,15 +9,11 @@ import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.processNextEventInCurrentThread
 import ru.practicum.android.diploma.databinding.FragmentSearchBinding
 import ru.practicum.android.diploma.feature.search.presentation.viewmodels.SearchViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import ru.practicum.android.diploma.R
-import ru.practicum.android.diploma.core.util.COUNTRY_KEY
 import ru.practicum.android.diploma.core.util.DataTransmitter
-import ru.practicum.android.diploma.databinding.LoadingItemBinding
-import ru.practicum.android.diploma.feature.filter.domain.model.FilterSettings
 import ru.practicum.android.diploma.feature.search.domain.VacanciesResponse
 import ru.practicum.android.diploma.feature.search.domain.models.VacancyShort
 import ru.practicum.android.diploma.feature.search.searchadapter.SlideInBottomAnimator
